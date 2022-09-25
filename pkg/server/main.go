19fa5d1dd55113404a60e9c51d52da0859e81fd1
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-09-25 20:07:16 +0700

    Refactor server listening logic out

---
+++ pkg/server/main.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+type Config struct {
+	Port int
+}
+
+type Server struct {
+	config     *Config
+	grpcServer *grpc.Server
+}
+
+func NewServer(config *Config, grpcServer *grpc.Server) *Server {
+	return &Server{
+		config:     config,
+		grpcServer: grpcServer,
+	}
+}
+
+func (s *Server) Start() error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.config.Port))
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		<-c
+		log.Printf("stopping...")
+		s.grpcServer.Stop()
+	}()
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.grpcServer.Serve(lis); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
--- pkg/user/cmd/server.go
+++ pkg/user/cmd/server.go
@@ -1,39 +1,21 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/dorm-parcel-manager/dpm/pkg/api"
+	"github.com/dorm-parcel-manager/dpm/pkg/server"
 	"github.com/dorm-parcel-manager/dpm/pkg/user/service"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
-	"log"
-	"net"
-	"os"
-	"os/signal"
 )
 
 func RunServer() error {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 4000))
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
 	s := grpc.NewServer()
 	api.RegisterUserServiceServer(s, service.NewUserServiceServer())
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
-	go func() {
-		<-c
-		log.Printf("stopping...")
-		s.Stop()
-	}()
-
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+	server := server.NewServer(&server.Config{Port: 4000}, s)
+	err := server.Start()
+	if err != nil {
 		return errors.WithStack(err)
 	}
-
 	return nil
 }
